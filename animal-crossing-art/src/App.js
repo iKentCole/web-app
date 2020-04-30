0341c256e43febbf3f7c885eb975df4641d38e24
Kent Coleman <Coleman.a.kent@gmail.com>
2020-04-29 20:11:53 -0600

    styled

--- animal-crossing-art/src/App.js
+++ animal-crossing-art/src/App.js
@@ -10,17 +10,21 @@ function App() {
       <div className="container">
         <div className="table top">
           <div className="row">
-            <input type="checkbox"></input>
+            <div className="checkbox-container">
+              <input type="checkbox"></input>
+            </div>
             <img src="https://gamewith-en.akamaized.net/article_tools/animal-crossing-new-horizons/gacha/a18_r.png"></img>
             <text>Academic Painting</text>
           </div>
         </div>
-        <h3 className="completed-label">
+        <h2 className="completed-label">
           Completed
-        </h3>
+        </h2>
         <div className="table bottom">
          <div className="row">
-            <input type="checkbox"></input>
+           <div className="checkbox-container">
+             <input type="checkbox"></input>
+           </div>
             <img src="https://gamewith-en.akamaized.net/article_tools/animal-crossing-new-horizons/gacha/a18_r.png"></img>
             <text>Academic Painting</text>
           </div>
